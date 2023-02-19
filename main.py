3c6a3a46408bdeae217e5cc03cda38bb3a173b23
Renat2492 <125185019+Renat2492@users.noreply.github.com>
2023-02-19 16:07:10 +0400

    Первоначальные файлы для проекта MakeCode

---
+++ main.py
@@ -0,0 +1,66 @@
+rand = 0
+players = 0
+current_player = 0
+step = -1
+
+def start_game():
+    global players, current_player, step
+    players = 2
+    current_player = 1
+    step = 0
+    basic.show_number(players)
+
+input.on_pin_pressed(TouchPin.P0, start_game)
+
+def on_button_pressed_a():
+    global players, step
+    if players > 2 and step == 0:
+        players = players -1
+        basic.show_number(players)
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global players, step
+    if step == 0:
+        players = players +1 
+        basic.show_number(players)
+
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_ab():
+    global step
+    step += 1 
+
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_gesture_shake():
+    global rand, players, current_player, step
+    if step > 0:
+        if current_player == 1:
+            basic.show_string("Round:" + str(step))
+        basic.show_string("Player:" + str(current_player))
+        rand = randint(0, 1)
+        if rand: 
+            basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            """)
+        else:
+            basic.show_leds("""
+            . # # # .
+            . # . # .
+            . # . # .
+            # # # # #
+            # . . . #
+            """)
+        if current_player < players:
+            current_player = current_player + 1
+        else:
+            current_player = 1
+            step = step + 1
+
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
---
+++ main.ts
@@ -0,0 +1,68 @@
+let rand = 0
+let players = 0
+let current_player = 0
+let step = -1
+input.onPinPressed(TouchPin.P0, function start_game() {
+    
+    players = 2
+    current_player = 1
+    step = 0
+    basic.showNumber(players)
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (players > 2 && step == 0) {
+        players = players - 1
+        basic.showNumber(players)
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (step == 0) {
+        players = players + 1
+        basic.showNumber(players)
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    step += 1
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    if (step > 0) {
+        if (current_player == 1) {
+            basic.showString("Round:" + ("" + step))
+        }
+        
+        basic.showString("Player:" + ("" + current_player))
+        rand = randint(0, 1)
+        if (rand) {
+            basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            `)
+        } else {
+            basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # . # .
+            # # # # #
+            # . . . #
+            `)
+        }
+        
+        if (current_player < players) {
+            current_player = current_player + 1
+        } else {
+            current_player = 1
+            step = step + 1
+        }
+        
+    }
+    
+})
